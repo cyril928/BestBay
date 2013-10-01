b4ebc036dbefe1c7233e95b897db3686eda98c64
Todd Sedano <todd.sedano@sv.cmu.edu>
2013-09-30 19:13:39 -0700

    Adding in initial repo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RisingFalcons::Application.config.secret_token = '0e317f94f8db142219952f9c8584de9241e498f79cd26bc916cd4372399c641f6e02f67acbfa5033dc1cc30d9328f0b8aa032fbf256fdf8673523a04b656390c'
