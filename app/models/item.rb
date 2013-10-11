f4309d48e03a845eb3381546e739b5d26b976347
Pratibha <pratibha25.anjali@gmail.com>
2013-10-11 12:34:26 -0400

    editied 2

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,5 @@
 class Item < ActiveRecord::Base
+  belongs_to :user
   attr_accessible :buyer_id, :category, :condition, :description, :duration, :price, :title, :user_id, :product
   has_attached_file :product, :styles => { :medium => "300x300>", :thumb => "100x100>" }
 end
