2050f7787b101bad07ac57703fc7f84ab322a4e2
Pratibha <pratibha25.anjali@gmail.com>
2013-11-17 12:51:57 -0500

    ready to merge

--- spec/mailer/Emailer_spec.rb
+++ spec/mailer/Emailer_spec.rb
@@ -1,13 +1,11 @@
 
 require 'spec_helper'
 
-
-
 describe Emailer do
   describe 'instructions' do
     let(:item) { mock_model(:item_id => '1', :title => "MyString", :category =>"MyString", :description => "MyString", :condition => "MyString", :duration =>12, :price =>1, :user_id => 1, :total_quantity => 10, :quantity => 5)}
-    let(:user) { mock_model(:email => 'lucas@email.com', :password => '12341234', :first_name =>'Tung-Keng Lee', :last_name => 'Lee', :street => '5700 Centre Ave.', :country => 'USA', :zip =>'15206', :phone => '4126801073') }
-    let(:mail) { Emailer.deliver_contact(1) }
+    let(:user) { mock_model(:userid => '2', :email => 'lucas@email.com', :password => '12341234', :first_name =>'Tung-Keng Lee', :last_name => 'Lee', :street => '5700 Centre Ave.', :country => 'USA', :zip =>'15206', :phone => '4126801073') }
+    let(:mail) { Emailer.deliver_contact(1, 2) }
 
     #ensure that the subject is correct
     it 'renders the subject' do
@@ -16,7 +14,7 @@ describe Emailer do
 
     #ensure that the receiver is correct
     it 'renders the receiver email' do
-      mail.to.should == ['risingfalcons19@gmail.com']
+      mail.to.should == ['bestbay2013fall@gmail.com']
     end
 
     #ensure that the sender is correct
