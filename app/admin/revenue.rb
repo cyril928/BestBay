03a1eea5c4e789a34b220f43fe06db2d46bca80a
Abhishek Mukhopadhyay <amukhop1@server.fake>
2013-12-01 15:16:22 -0500

    Fixed revenue view in admin to fix a bug in show due to wrong relations on the model. Also added a total revenue field in admin view.

--- app/admin/revenue.rb
+++ app/admin/revenue.rb
@@ -1,3 +1,19 @@
 ActiveAdmin.register Revenue do
-
+  index do
+    total_revenue = 0
+    column :id
+    column :name
+    column :card_number
+    column :expiry_date
+    column :address
+    column :amount
+    column :is_transaction_revenue
+    column :created_at
+    column :updated_at
+    column ("Cumulative Revenue") {|resource| total_revenue += resource.amount}
+    default_actions
+    div :class =>"panel" do
+      h3 "Total Revenue: #{total_revenue}"
+    end
+  end
 end
