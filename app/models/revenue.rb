29f5b2fb6cc7e46141ef76142cbc0e4bbf2298e2
Abhishek Mukhopadhyay <amukhop1@server.fake>
2013-12-06 13:48:35 -0500

    Revenue redone which was lost in merge

--- app/models/revenue.rb
+++ app/models/revenue.rb
@@ -1,6 +1,4 @@
 class Revenue < ActiveRecord::Base
-  has_one :user
-  has_one :item
   attr_accessible :address, :amount, :card_number, :expiry_date, :is_transaction_revenue, :item_id, :name, :user_id
 
   begin
